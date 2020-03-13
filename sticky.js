00f2ed32d92c0c5290c2fc0452ff263a163af8a2
Colin Jaffe <balloonasaurus@gmail.com>
2020-03-13 14:39:44 -0400

    Adjusts transition animation.

--- sticky.js
+++ sticky.js
@@ -2,14 +2,14 @@
 window.addEventListener('scroll', () => {
   const body = document.querySelector('body');
   const photoArea = document.querySelector('.photo-area');
-  const nav = document.querySelector('.nav');
+  const navArea = document.querySelector('.nav-area');
   if (window.scrollY >= 60) {
     body.classList.add('sticky');
-    nav.classList.add('sticky');
+    navArea.classList.add('sticky');
     photoArea.classList.add('sticky');
   } else {
     body.classList.remove('sticky');
-    nav.classList.remove('sticky');
+    navArea.classList.remove('sticky');
     photoArea.classList.remove('sticky');
   }
 });
