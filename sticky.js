710a9464f128b0ff3e77cdf021a30acc2930e7d9
Colin Jaffe <balloonasaurus@gmail.com>
2021-08-29 14:32:24 -0400

    Ran prettier.

--- sticky.js
+++ sticky.js
@@ -1,14 +1,14 @@
-window.addEventListener('scroll', () => {
-  const body = document.querySelector('body');
-  const photoArea = document.querySelector('.photo-area');
-  const navArea = document.querySelector('.nav-area');
-    body.classList.add('sticky');
-    navArea.classList.add('sticky');
-    photoArea.classList.add('sticky');
+window.addEventListener("scroll", () => {
+  const body = document.querySelector("body");
+  const photoArea = document.querySelector(".photo-area");
+  const navArea = document.querySelector(".nav-area");
   if (window.scrollY >= 20) {
+    body.classList.add("sticky");
+    navArea.classList.add("sticky");
+    photoArea.classList.add("sticky");
   } else {
-    body.classList.remove('sticky');
-    navArea.classList.remove('sticky');
-    photoArea.classList.remove('sticky');
+    body.classList.remove("sticky");
+    navArea.classList.remove("sticky");
+    photoArea.classList.remove("sticky");
   }
 });
