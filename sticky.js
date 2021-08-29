1b4a668cbbc538644030ec89e282b39fe500c1be
Colin Jaffe <balloonasaurus@gmail.com>
2021-08-29 14:32:00 -0400

    Sped up photo-shrinking transition.

--- sticky.js
+++ sticky.js
@@ -2,10 +2,10 @@ window.addEventListener('scroll', () => {
   const body = document.querySelector('body');
   const photoArea = document.querySelector('.photo-area');
   const navArea = document.querySelector('.nav-area');
-  if (window.scrollY >= 60) {
     body.classList.add('sticky');
     navArea.classList.add('sticky');
     photoArea.classList.add('sticky');
+  if (window.scrollY >= 20) {
   } else {
     body.classList.remove('sticky');
     navArea.classList.remove('sticky');
